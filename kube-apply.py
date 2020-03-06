7ac747b34be25ad78565f036d3ccfad361586604
Denis Loginov <denis@broadinstitute.org>
2020-03-06 14:49:59 -0500

    [kube-apply] Fix input encoding for Python 3.5

--- kube-apply.py
+++ kube-apply.py
@@ -5,8 +5,7 @@ import os, subprocess, sys
 with open(sys.argv[1]) as f:
   subprocess.run(
     ['kubectl', 'apply', '-f', '-'],
-    input=os.path.expandvars(f.read()),
+    input=os.path.expandvars(f.read()).encode(),
     stdout=sys.stdout,
     stderr=sys.stderr,
-    encoding='ascii',
   ).check_returncode()
