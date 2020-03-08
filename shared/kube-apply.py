847fbb256e07ff8758997ce830eafe0ca4e8e950
Denis Loginov <denis@broadinstitute.org>
2020-03-08 00:36:07 -0500

    [shared] Fix template encoding in kube-apply.py

--- shared/kube-apply.py
+++ shared/kube-apply.py
@@ -4,13 +4,13 @@ import os, re, subprocess, sys
 
 for name in sys.argv[1:]:
   with open(name) as f:
-    template = os.path.expandvars(f.read()).encode()
+    template = os.path.expandvars(f.read())
     undefined = re.findall(r'\${[^{]+}', template)
     if undefined:
       raise 'Undefined variable(s) in ' + name + ': ' + undefined
     subprocess.run(
       ['kubectl', 'apply', '-f', '-'],
-      input=template,
+      input=template.encode(),
       stdout=sys.stdout,
       stderr=sys.stderr,
     ).check_returncode()
