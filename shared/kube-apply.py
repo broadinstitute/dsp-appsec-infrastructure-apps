196686016263557f906d56f7a37a1d6f06c58efc
Denis Loginov <denis@broadinstitute.org>
2020-03-08 00:21:43 -0500

    [shared] Check for undefined variales in kube-apply.py

--- shared/kube-apply.py
+++ shared/kube-apply.py
@@ -4,9 +4,13 @@ import os, subprocess, sys
 
 for name in sys.argv[1:]:
   with open(name) as f:
+    template = os.path.expandvars(f.read()).encode()
+    undefined = re.findall(r'\${[^{]+}', template):
+    if undefined:
+      raise 'Undefined variable(s) in ' + name + ': ' + undefined
     subprocess.run(
       ['kubectl', 'apply', '-f', '-'],
-      input=os.path.expandvars(f.read()).encode(),
+      input=template,
       stdout=sys.stdout,
       stderr=sys.stderr,
     ).check_returncode()
