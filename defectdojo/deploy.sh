3dbaf43f0fbb23926ddf0b188d6e99999486d19b
Denis Loginov <denis@broadinstitute.org>
2020-03-09 21:12:50 -0400

    [defectdojo] Set up SQL proxy for initializer Job

--- defectdojo/deploy.sh
+++ defectdojo/deploy.sh
@@ -41,8 +41,9 @@ export DJANGO_CONFIG="django"
 export DD_DATABASE_USER="postgres"
 export DD_DATABASE_PORT="5432"
 
-export SQL_INSTANCE="${NAMESPACE}-sql"
 export SQL_REGION="us-east1"
+export SQL_INSTANCE="${NAMESPACE}-sql"
+export SQL_INSTANCE_URI="${PROJECT_ID}:${SQL_REGION}:${SQL_INSTANCE}=tcp:${DD_DATABASE_PORT}"
 
 export IP_NAME="${NAMESPACE}"
 export LOCALHOST="127.0.0.1"
