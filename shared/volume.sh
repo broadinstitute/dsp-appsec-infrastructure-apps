b65b33280b3d4ddb3dbceb121775961f0085a75f
Denis Loginov <denis@broadinstitute.org>
2020-03-14 01:07:30 -0400

    [shared] Fix path to kube-apply script

--- shared/volume.sh
+++ shared/volume.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 export COMPUTE_DISK=${NAMESPACE}
 
-./kube-apply.sh "disk.yaml"
+./kube-apply.py "disk.yaml"
 
 kubectl wait --for condition=Ready ComputeDisk \
     "${COMPUTE_DISK}" -n "${NAMESPACE}"
 
-./kube-apply.sh "volume.yaml"
+./kube-apply.py "volume.yaml"
