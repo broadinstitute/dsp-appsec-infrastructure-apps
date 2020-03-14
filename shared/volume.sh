703a7436e6864a762e12843b4f084ce4aab6b399
Denis Loginov <denis@broadinstitute.org>
2020-03-14 01:13:22 -0400

    [shared] Wait longer for disk creation

--- shared/volume.sh
+++ shared/volume.sh
@@ -7,6 +7,6 @@ export COMPUTE_DISK=${NAMESPACE}
 ./kube-apply.py "disk.yaml"
 
 kubectl wait --for condition=Ready ComputeDisk \
-    "${COMPUTE_DISK}" -n "${NAMESPACE}"
+    "${COMPUTE_DISK}" -n "${NAMESPACE}" --timeout "120s"
 
 ./kube-apply.py "volume.yaml"
