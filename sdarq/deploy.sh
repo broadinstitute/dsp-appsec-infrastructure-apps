627ea0946d3330b26a8a112eeb97378b006d0614
Zybjana Bedo <zbedo@broadinstitute.org>
2020-03-17 16:45:57 -0400

    Add sdarq deployment files

---
+++ sdarq/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CWD="${PWD}"
+cd ../shared
+
+# Create and switch to the namespace
+
+export PROJECT_ID="$(gcloud config get-value project)"
+export NAMESPACE="sdarq"
+
+./kube-apply.sh "namespace.yaml"
+
+export SDARQ_SECRET="sdarq"
+
+# Deploy the service
+
+export DEPLOYMENT="${NAMESPACE}"
+
+export IP_NAME="${NAMESPACE}"
+export DNS_HOSTNAME="${NAMESPACE}.${DNS_DOMAIN}"
+
+export INGRESS="${SERVICE}"
+export MANAGED_CERT="${SERVICE}"
+export BACKEND_CONFIG="${SERVICE}"
+export FRONTEND_PORT="frontend"
+export BACKEND_PORT="backend"
+
+./kube-apply.py "${CWD}/deployment.yaml"
+
+./host.sh
+
+./kube-apply.py "${CWD}/ingress.yaml"
