6a1c6c408ec0a501b5c2f605925c8e23e4520129
Denis Loginov <denis@broadinstitute.org>
2020-04-05 17:21:18 -0400

    Add script and templates for batch dispatcher deployment

--- batch/entrypoint.py
+++ batch/entrypoint.py
@@ -3,48 +3,46 @@
 from google.cloud import pubsub_v1
 from google.cloud.pubsub_v1.subscriber.message import Message
 from hashlib import md5
-from kubernetes.client import BatchV1Api, V1Job
+from kubernetes.client import BatchV1Api, V1Job, V1ObjectMeta
 from kubernetes.config import load_kube_config
 import logging as log
-from os import environ, path
-import re
+from os import environ
 from typing import Callable
 import yaml
 
 
-def render_job(tmpl: str, job_id: str, job_input: str) -> V1Job:
-    environ['JOB_ID'] = job_id
-    environ['JOB_INPUT'] = job_input
+def render_job(name: str, subscription: str, spec: str, job_input: str) -> V1Job:
+    metadata = V1ObjectMeta(name=name)
+    spec = spec.format(JOB_INPUT=job_input)
+    spec = yaml.safe_load(spec)
+    return V1Job(
+        api_version='batch/v1',
+        kind='Job',
+        metadata=metadata,
+        spec=spec,
+    )
 
-    tmpl = path.expandvars(tmpl)
-    undef = re.findall(r'\${[^{]+}', tmpl)
-    if undef:
-        raise ValueError(
-            f'Undefined variable(s) in {tmpl}: {undef}'
-        )
-    return yaml.safe_load(tmpl)
 
-
-def callback(batch_v1: BatchV1Api, namespace: str, tmpl: str) -> Callable[[Message], None]:
+def callback(batch_v1: BatchV1Api, namespace: str, spec: str) -> Callable[[Message], None]:
     def cb(m: Message):
         try:
-            job_id = md5(m.message_id.encode('utf-8')).hexdigest()
+            job_name = md5(m.message_id.encode('utf-8')).hexdigest()
             job_input = m.data.decode('utf-8')
-            log.info(f'Submitting job {job_id} with input "{job_input}"')
+            log.info(f'Submitting job {job_name} with input "{job_input}"')
 
-            job = render_job(tmpl, job_id, job_input)
+            job = render_job(job_name, spec, job_input)
             batch_v1.create_namespaced_job(namespace, job)
-            log.info(f'Submitted job {job_id}')
+            log.info(f'Submitted job {job_name}')
         except Exception:
             log.exception('PubSub subscriber callback')
         m.ack()
     return cb
 
 
-def listen(subscription: str, batch_v1: BatchV1Api, namespace: str, tmpl: str) -> None:
+def listen(subscription: str, batch_v1: BatchV1Api, namespace: str, spec: str) -> None:
     subscriber = pubsub_v1.SubscriberClient()
     with subscriber:
-        cb = callback(batch_v1, namespace, tmpl)
+        cb = callback(batch_v1, namespace, spec)
         streaming_pull = subscriber.subscribe(subscription, cb)
         log.info(f'Listening to subscription {subscription}')
         try:
@@ -54,25 +52,25 @@ def listen(subscription: str, batch_v1: BatchV1Api, namespace: str, tmpl: str) -
             raise e
 
 
-def load_job_template(tmpl_path: str) -> str:
-    with open(tmpl_path) as f:
+def load_job_spec(spec_path: str) -> str:
+    with open(spec_path) as f:
         return f.read()
 
 
 def main():
-    subscription = environ['SUBSCRIPTION']
     namespace = environ['NAMESPACE']
-    tmpl_path = environ['TEMPLATE_PATH']
+    subscription = environ['SUBSCRIPTION']
+    spec_path = environ['SPEC_PATH']
     log_level = environ.get('LOG_LEVEL', 'INFO')
 
     log.basicConfig(level=log_level)
 
-    tmpl = load_job_template(tmpl_path)
+    spec = load_job_spec(spec_path)
 
     load_kube_config()
     batch_v1 = BatchV1Api()
 
-    listen(subscription, batch_v1, namespace, tmpl)
+    listen(subscription, batch_v1, namespace, spec)
 
 
 if __name__ == '__main__':
---
+++ shared/batch.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+###
+# This script deploys batch Job dispatcher container,
+# which listens to a PubSub topic for job inputs
+# and submits Kubernetes Jobs with those.
+#
+# Input variables:
+#   PROJECT_ID - GCP project ID
+#   NAMESPACE - namespace for deployment
+#   BATCH_DISPATCHER_IMAGE - Docker image for the dispatcher
+#   JOB_CREATOR_ROLE - ClusterRole for creating Jobs
+#   JOB_TOPIC - PubSub topic name to be created by this script
+#   JOB_CONFIG_MAP - name of the ConfigMap storing the Job spec
+#
+# JOB_CONFIG_MAP must store Job spec as a string under 'spec' key.
+#
+# Job spec must contain a literal {JOB_INPUT},
+# which will be replaced with the contents
+# of an incoming PubSub message,
+# and it is up to the Job how to interpret it
+# (for example, you can pass it as
+# a command-line arg, or an env variable).
+#
+
+export JOB_SUBSCRIPTION="${JOB_TOPIC}"
+export JOB_DEPLOYMENT="${JOB_TOPIC}"
+export JOB_CONFIG_MAP="${JOB_DEPLOYMENT}"
+export JOB_CONFIG_VOLUME="job-config"
+export JOB_CONFIG_MOUNT_PATH="/job"
+export JOB_SPEC_KEY="spec"
+
+export SERVICE="${JOB_DEPLOYMENT}"
+export SERVICE_ACCOUNT="${SERVICE}"
+
+./kube-apply.py "service-account.yaml" "batch.yaml"
