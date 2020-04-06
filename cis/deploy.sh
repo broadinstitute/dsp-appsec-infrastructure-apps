4921492d7d327f3a00ed2dc27b20f81a6889eff5
Denis Loginov <denis@broadinstitute.org>
2020-04-06 17:42:04 -0400

    [cis] Configure batch job dispatcher for CIS scans

--- cis/deploy.sh
+++ cis/deploy.sh
@@ -6,7 +6,12 @@ CWD="${PWD}"
 cd ../shared
 
 export NAMESPACE="cis"
+export JOB_TOPIC="${NAMESPACE}-scans"
+export JOB_CONFIG_MAP="${JOB_TOPIC}"
+export K8S_SERVICE_ACCOUNT="${JOB_TOPIC}"
 
 ./kube-apply.py \
   "namespace.yaml" \
   "${CWD}/deployment.yaml"
+
+./batch.sh
