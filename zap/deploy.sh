d36283c27fc194b45742f1775d7612d9f2f4b521
Denis Loginov <denis@broadinstitute.org>
2020-04-08 19:31:09 -0400

    [zap] Fix env assignment order

--- zap/deploy.sh
+++ zap/deploy.sh
@@ -6,12 +6,11 @@ CWD="${PWD}"
 cd ../shared
 
 export NAMESPACE="zap"
+export JOB_TOPIC="${NAMESPACE}-scans"
+export JOB_CONFIG_MAP="${JOB_TOPIC}"
 
 ./kube-apply.py \
   "namespace.yaml" \
   "${CWD}/deployment.yaml"
-
-export JOB_TOPIC="${NAMESPACE}-scans"
-export JOB_CONFIG_MAP="${JOB_TOPIC}"
 
 ./batch.sh
