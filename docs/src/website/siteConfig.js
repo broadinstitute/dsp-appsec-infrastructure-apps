54160e0bfd01cff8e21efc71871b3d50a7aead08
Denis Loginov <denis@broadinstitute.org>
2020-04-14 19:51:09 -0400

    [docs] Set repoUrl

--- docs/src/website/siteConfig.js
+++ docs/src/website/siteConfig.js
@@ -117,7 +117,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl: 'https://github.com/broadinstitute/dsp-appsec-infrastructure-apps',
 };
 
 module.exports = siteConfig;
