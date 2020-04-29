203e83c751ccf9b56a67711e3c38d24bd7c9bc6f
Zybjana Bedo <zbedo@broadinstitute.org>
2020-04-29 14:22:41 -0400

    [sdarq] Code style formatting for app.module and app.routing

--- sdarq/frontend/src/app/app-routing.module.ts
+++ sdarq/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
-import { FormComponent} from './form/form.component';
-import { NonDSPformComponent} from './non-dspform/non-dspform.component';
+import { FormComponent } from './form/form.component';
+import { NonDSPformComponent } from './non-dspform/non-dspform.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CisComponent } from './cis/cis.component';
 import { ScanpageComponent } from './scanpage/scanpage.component';
@@ -10,18 +10,16 @@ import { CisResultsComponent } from './cis-results/cis-results.component';
 import { CisScanComponent } from './cis-scan/cis-scan.component';
 import { CisLandingPageComponent } from './cis-landing-page/cis-landing-page.component';
 
-
-
 const routes: Routes = [
-  { path: '', component: HomepageComponent},
-  { path: 'newservice', component: MainpageComponent},
-  { path: 'questionnaire', component: FormComponent},
-  { path: 'notdspquestionnaire', component: NonDSPformComponent},
-  { path: 'cis/getresults', component: CisComponent},
-  { path: 'scan', component: ScanpageComponent},
-  { path: 'cis/results', component: CisResultsComponent},
-  { path: 'cis/scan', component: CisScanComponent},
-  { path: 'cis', component: CisLandingPageComponent}
+  { path: '', component: HomepageComponent },
+  { path: 'newservice', component: MainpageComponent },
+  { path: 'questionnaire', component: FormComponent },
+  { path: 'notdspquestionnaire', component: NonDSPformComponent },
+  { path: 'cis/getresults', component: CisComponent },
+  { path: 'scan', component: ScanpageComponent },
+  { path: 'cis/results', component: CisResultsComponent },
+  { path: 'cis/scan', component: CisScanComponent },
+  { path: 'cis', component: CisLandingPageComponent }
 ];
 
 @NgModule({
--- sdarq/frontend/src/app/app.module.ts
+++ sdarq/frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { SurveyComponent } from './survey.component';
 import { SurveyCreatorComponent } from './survey.creator.component';
@@ -13,11 +12,11 @@ import { SendFormDataService } from './services/send-form-data.service';
 import { NonDSPformComponent } from './non-dspform/non-dspform.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CisComponent } from './cis/cis.component';
-import { CisProjectService} from './services/cis-project.service';
+import { CisProjectService } from './services/cis-project.service';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ScanpageComponent } from './scanpage/scanpage.component';
-import { GetCisScanService} from './services/get-cis-scan.service';
+import { GetCisScanService } from './services/get-cis-scan.service';
 import { FooterComponent } from './footer/footer.component';
 import { CisResultsComponent } from './cis-results/cis-results.component';
 import { CisScanComponent } from './cis-scan/cis-scan.component';
@@ -53,9 +52,9 @@ import { CisLandingPageComponent } from './cis-landing-page/cis-landing-page.com
     SendFormDataService,
     CisProjectService,
     GetCisScanService
-    ],
+  ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule { }
