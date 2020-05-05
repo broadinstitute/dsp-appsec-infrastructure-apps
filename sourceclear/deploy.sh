2a101733db83a8fa0496d0a81463f332d6363059
Sarada Symonds <ssymonds@broadinstitute.org>
2020-05-05 11:35:18 -0400

    service account and pub sub kubernetes env vars in deploy.sh

--- sourceclear/deploy.sh
+++ sourceclear/deploy.sh
@@ -10,6 +10,7 @@ python3 render_template.py
 export NAMESPACE="sourceclear"
 export JOB_TOPIC="${NAMESPACE}-scans"
 export JOB_CONFIG_MAP="${JOB_TOPIC}"
+export SERVICE_ACCOUNT="${NAMESPACE}"
 
 ./kube-apply.py \
   "namespace.yaml" \
--- sourceclear/render_template.py
+++ sourceclear/render_template.py
@@ -1,8 +1,12 @@
 from jinja2 import Template
 import sys 
 
-with open("./templates/dsde_cronjobs.yaml.jinja2", "r") as job_input: 
+with open("./templates/config_map.yaml.jinja2", "r") as job_input: 
     with open("./deployment.yaml", "w") as f: 
+        f.write(Template(job_input.read()).render())
+
+with open("./templates/dsde_cronjobs.yaml.jinja2", "r") as job_input: 
+    with open("./deployment.yaml", "a") as f: 
         f.write(Template(job_input.read()).render())
 
 with open("./templates/kdux_cronjobs.yaml.jinja2", "r") as job_input: 
