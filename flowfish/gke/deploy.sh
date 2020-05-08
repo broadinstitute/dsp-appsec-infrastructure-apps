808d780a4ffefb62747872e7f1cdcc905cf65087
Albano Drazhi <adrazhi@broadinstitute.org>
2020-05-08 02:06:36 -0400

    [flowfish] Add gke deploy script

--- flowfish/gke/deploy.sh
+++ flowfish/gke/deploy.sh
@@ -2,4 +2,16 @@
 
 set -euo pipefail
 
-# ToDo
+CWD="${PWD}"
+cd ../shared
+
+export NAMESPACE="flowfish"
+export JOB_TOPIC="${NAMESPACE}-scans"
+export JOB_CONFIG_MAP="${JOB_TOPIC}"
+export K8S_SERVICE_ACCOUNT="${JOB_TOPIC}"
+
+./kube-apply.py \
+  "namespace.yaml" \
+  "${CWD}/deployment.yaml"
+
+./batch.sh
