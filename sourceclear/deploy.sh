3465ec80d7778fef9a117929dc34b5d857c37db3
Sarada Symonds <ssymonds@broadinstitute.org>
2020-05-08 12:42:10 -0400

    moved pubsub script to python

--- sourceclear/deploy.sh
+++ sourceclear/deploy.sh
@@ -11,6 +11,9 @@ export NAMESPACE="sourceclear"
 export JOB_TOPIC="${NAMESPACE}-scans"
 export JOB_CONFIG_MAP="${JOB_TOPIC}"
 export SERVICE_ACCOUNT="${NAMESPACE}"
+export SRCCLR_DATASET="cis"
+export SRCCLR_TABLE="dsp_appsec_${NAMESPACE}_repos"
+
 
 ./kube-apply.py \
   "namespace.yaml" \
--- sourceclear/docker/setup.sh
+++ sourceclear/docker/setup.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 apt-get update && \
-apt-get install -y nodejs && \
-apt-get -y install nodejs npm && \
+apt-get install -y nodejs \
+    npm \
+    zip \
+    maven \
+    openjdk-11-jdk sbt \
+    wget \
+    git \
+    python3-pip
+
+pip3 install --upgrade google-cloud-bigquery google-cloud-pubsub google-cloud-resource-manager
+
+# install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
-apt install yarn -y yarn && \
-apt-get install -y zip && \
-apt-get install -y maven && \
-apt-get install -y openjdk-11-jdk sbt && \
-apt-get install -y wget && \
+apt install yarn -y yarn
+
+# install scala
 wget http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.deb && \
 dpkg -i scala-2.11.8.deb && \
 apt-get install -y scala && \
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list && \
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add && \
 apt-get update && \
-apt-get install -y sbt && \
-apt-get install -y git
-apt-get install -y jq
+apt-get install -y sbt
