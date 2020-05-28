41ff40d6d1d702b1f710f6022ddd3cb00915a489
Denis Loginov <denis@broadinstitute.org>
2020-05-28 12:59:20 -0400

    [sdarq/cis] Display impact score on frontend

--- sdarq/frontend/src/app/pipes/filter.pipe.ts
+++ sdarq/frontend/src/app/pipes/filter.pipe.ts
@@ -1,24 +1,30 @@
-import { Pipe, PipeTransform, Injectable } from '@angular/core';
+import { Pipe, PipeTransform, Injectable } from "@angular/core";
 
 @Pipe({
-    name: 'filter'
+  name: "filter",
 })
 @Injectable()
 export class FilterPipe implements PipeTransform {
-    transform(value: any[], searchString: string): any[] {
-
-        if (!searchString) {
-            return value
-        }
-        if (searchString) {
-            return value.filter(it => {
-                const id = it.id.toString().includes(searchString)
-                const level = it.level.toString().includes(searchString)
-                const title = it.title.toLowerCase().includes(searchString.toLowerCase())
-                const description = it.description.toLowerCase().includes(searchString.toLowerCase())
-                const rationale = it.rationale.toLowerCase().includes(searchString.toLowerCase())
-                return (id + level + title + description + rationale);
-            })
-        }
+  transform(value: any[], searchString: string): any[] {
+    if (!searchString) {
+      return value;
     }
+    if (searchString) {
+      return value.filter((it) => {
+        const id = it.id.toString().includes(searchString);
+        const level = it.level.toString().includes(searchString);
+        const impact = it.impact.toString().includes(searchString);
+        const title = it.title
+          .toLowerCase()
+          .includes(searchString.toLowerCase());
+        const description = it.description
+          .toLowerCase()
+          .includes(searchString.toLowerCase());
+        const rationale = it.rationale
+          .toLowerCase()
+          .includes(searchString.toLowerCase());
+        return id + level + impact + title + description + rationale;
+      });
+    }
+  }
 }
