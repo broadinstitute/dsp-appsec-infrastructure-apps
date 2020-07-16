e65ed76856e9d19172b575432ebbc1de3645528b
Sarada Symonds <ssymonds@broadinstitute.org>
2020-07-16 17:17:32 -0400

    codedx url issue

--- codedx-analysis/deploy.sh
+++ codedx-analysis/deploy.sh
@@ -8,7 +8,7 @@ cd ../shared
 export NAMESPACE="codedx-analysis"
 
 ./kube-apply.py "namespace.yaml"
-export CODEDX_URL = "http://codedx.codedx.svc.cluster.local"
+export CODEDX_URL="http://codedx.codedx.svc.cluster.local"
 export SECRET_NAME="codedx-api-key"
 export SECRET_VERSION="1"
 export JOB_DEPLOYMENT="${NAMESPACE}"
