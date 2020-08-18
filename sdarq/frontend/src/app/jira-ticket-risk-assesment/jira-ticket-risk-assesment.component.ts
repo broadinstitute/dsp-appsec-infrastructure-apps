e588ab5b55449003dc1aab845db835a7bf726862
Zybjana Bedo <zbedo@broadinstitute.org>
2020-08-18 12:11:02 -0400

    [sdarq] Add new json form, remove endpoint from backend and ingress file

--- sdarq/frontend/src/app/jira-ticket-risk-assesment/jira-ticket-risk-assesment.component.ts
+++ sdarq/frontend/src/app/jira-ticket-risk-assesment/jira-ticket-risk-assesment.component.ts
@@ -11,46 +11,11 @@ import formJson from './form.json';
 })
 export class JiraTicketRiskAssesmentComponent implements OnInit {
 
-  risk: any;
-  data: any[];
-  showSpinner: boolean;
-  showModal_low: boolean;
-  showModal_medium: boolean;
-  showModal_high: boolean;
-  showForm: boolean;
-
   constructor(private sendJiraForm: SendJiraRiskDataService, private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.showModal_low = false;
-    this.showModal_medium = false;
-    this.showModal_high = false;
-    this.showForm = true;
-  }
+  ngOnInit(): void { }
 
   json = formJson
 
-  sendData(result) {
-    this.sendJiraForm.sendJiraData(result).subscribe((data: any) => {
-      this.risk = data;
-      this.showForm = false;
-      this.showSpinner = false;
-      if (this.risk.Risk == 'Low') {
-        this.showModal_low = true;
-      }
-      else if (this.risk.Risk == 'Medium') {
-        this.showModal_medium = true;
-      }
-      else {
-        this.showModal_high = true
-      }
-    },
-      (data) => {
-        this.showModal_low = false;
-        this.showModal_medium = false;
-        this.showModal_high = false;
-        this.showForm = false;
-        this.showSpinner = true;
-      });
-  }
+  sendData(result) { }
 }
