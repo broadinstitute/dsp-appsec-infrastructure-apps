842c7031cccf4a60735cf0a9a90f7b0ed26f1f89
Sarada Symonds <ssymonds@broadinstitute.org>
2020-08-27 17:30:30 -0400

    datetime support

--- codedx-analysis/src/worker.py
+++ codedx-analysis/src/worker.py
@@ -6,6 +6,8 @@ import os
 from codedx_api import CodeDxAPI
 from google.cloud import pubsub_v1, storage
 from slack import WebhookClient
+from dateutil import tz, parser
+from datetime import datetime
 
 codedx_api_key = os.environ['codedx_api_key']
 project_id = os.environ['PROJECT_ID']
@@ -25,13 +27,22 @@ def callback(message):
         object_metadata = json.loads(data)
         obj_path = object_metadata['name']
         bucket_name = object_metadata['bucket']
-        slack_text = "New vulnerability report detected in GCS bucket: \
-                    https://console.cloud.google.com/storage/browser/{}/{}".format(bucket_name, obj_path)
-        response = webhook.send(text=slack_text)
 
         source_blob_name = obj_path
         file_name = source_blob_name.split("/")[-1]
         project = object_metadata["metadata"]["project"]
+
+        cdx = CodeDxAPI.CodeDx(base_url, codedx_api_key)
+        analysis = cdx.get_all_analysis(project)
+        print(analysis)
+        prev_analysis = parser.parse(analysis[-1]["creationTime"])
+        now = datetime.now(tz.tzlocal())
+        if (now - prev_analysis).total_seconds() < 5.0:
+            return
+        
+        slack_text = "New vulnerability report detected in GCS bucket: \
+            https://console.cloud.google.com/storage/browser/{}/{}".format(bucket_name, obj_path)
+        response = webhook.send(text=slack_text)
 
         # Download file
         storage_client = storage.Client()
@@ -40,7 +51,6 @@ def callback(message):
         blob.download_to_filename(file_name)
 
         # Upload Zap report to Codedx
-        cdx = CodeDxAPI.CodeDx(base_url, codedx_api_key)
         cdx.update_projects()
         if project not in list(cdx.projects):
             cdx.create_project(project)
