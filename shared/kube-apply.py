07166f21cf18e60ef0f5bd0160623f61c7099484
Denis Loginov <dinvlad@gmail.com>
2021-01-16 17:23:45 -0500

    [cloudbuild] Set up https_proxy via .env

--- shared/kube-apply.py
+++ shared/kube-apply.py
@@ -5,10 +5,6 @@ import re
 import subprocess
 import sys
 
-https_proxy = "socks5://" + \
-    os.environ["PROXY_HOST"] + ":" + os.environ["PROXY_PORT"]
-env = dict(os.environ, https_proxy=https_proxy)
-
 for name in sys.argv[1:]:
     with open(name) as f:
         template = os.path.expandvars(f.read())
@@ -18,10 +14,9 @@ for name in sys.argv[1:]:
                 'Undefined variable(s) in {}: {}'.format(name, undefined)
             )
         subprocess.run(
-            ['kubectl', 'apply', '--request-timeout=1m', '-f', '-'],
+            ['kubectl', 'apply', '-f', '-'],
             input=template.encode(),
             stdout=sys.stdout,
             stderr=sys.stderr,
-            env=env,
             check=True,
         )
