91be69cce97383c0035387a91961a455d0da91da
Denis Loginov <dinvlad@gmail.com>
2021-02-02 16:47:22 -0500

    [zap] Fix SA assignments

--- zap/deploy.sh
+++ zap/deploy.sh
@@ -10,7 +10,8 @@ export NAMESPACE="zap"
 export JOB_TOPIC="${NAMESPACE}-scans"
 export JOB_CONFIG_MAP="${JOB_TOPIC}"
 export JOB_SECRET="${JOB_CONFIG_MAP}"
-export K8S_SERVICE_ACCOUNT="${JOB_CONFIG_MAP}"
+export JOB_SERVICE_ACCOUNT="${JOB_CONFIG_MAP}"
+export CRON_SERVICE_ACCOUNT="${NAMESPACE}-trigger"
 export CODEDX_URL="http://${CODEDX_NAMESPACE}.${CODEDX_SERVICE}.svc.cluster.local/codedx"
 export DEFECT_DOJO_URL="http://${DOJO_NAMESPACE}.${DOJO_SERVICE}.svc.cluster.local"
 export ZAP_PORT='8008'
@@ -18,7 +19,14 @@ export BUCKET_NAME="${PROJECT_ID}-vuln-reports"
 
 ./kube-apply.py \
   "namespace.yaml" \
-  "configconnectorcontext.yaml" \
-  "${CWD}/deployment.yaml"
+  "configconnectorcontext.yaml"
+
+export SERVICE_ACCOUNT="${JOB_SERVICE_ACCOUNT}"
+./kube-apply.py "service-account.yaml"
+
+export SERVICE_ACCOUNT="${CRON_SERVICE_ACCOUNT}"
+./kube-apply.py "service-account.yaml"
+
+./kube-apply.py "${CWD}/deployment.yaml"
 
 ./batch.sh
