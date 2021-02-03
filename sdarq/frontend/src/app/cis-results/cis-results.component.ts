6175457a41e1ec897650866018f3aaecac46f614
Zybjana Bedo <zbedo@broadinstitute.org>
2021-02-03 16:37:43 -0500

    [sdarq][frontend] Fix lint error

--- sdarq/frontend/src/app/cis-results/cis-results.component.ts
+++ sdarq/frontend/src/app/cis-results/cis-results.component.ts
@@ -53,7 +53,7 @@ export class CisResultsComponent implements OnInit {
 
   ConvertToCSV(json: string, fields: any): string {
 
-    const Json2csvParser = require('json2csv').parse; 
+    const Json2csvParser = require('json2csv').parse;
     let options: {
     };
     options = fields;
