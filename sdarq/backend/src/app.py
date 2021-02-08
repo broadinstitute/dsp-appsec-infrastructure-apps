cab7fa43571955c6827b0742c7e1f8cd1ce8ea72
Zybjana Bedo <44617524+zbedo@users.noreply.github.com>
2021-02-08 18:02:32 -0500

    Verify /submit endpoint (#129)
    * [sdarq][frontend] Move from DefectDojo v1 to V2
    
    * [sdarq][backend] Fix slack channel list
    
    * [sdarq][frontend] Add DSP Data Centers in Jira projects
    
    * [sdarq][backend]
    
    * [sdarq] DSPDC from Jira project lists
    
    * [sdarq][test] Verify /submit endpoint

--- sdarq/backend/src/app.py
+++ sdarq/backend/src/app.py
@@ -43,7 +43,6 @@ jira_instance = os.getenv('jira_instance')
 sdarq_host = os.getenv('sdarq_host')
 dojo_host_url = os.getenv('dojo_host_url')
 firestore_collection = os.getenv('firestore_collection')
-# appsec_jira_board = os.getenv('appsec_jira_board')
 topic_name = os.environ['JOB_TOPIC']
 pubsub_project_id = os.environ['PUBSUB_PROJECT_ID']
 
@@ -106,7 +105,7 @@ def submit():
     appsec_jira_ticket_description = github_url + '\n' + architecture_diagram
     appsec_jira_ticket_summury = 'Threat Model request ' + dojo_name
 
-    jira_ticket_appsec = jira.create_issue(project='DSEC',
+    jira_ticket_appsec = jira.create_issue(project='ATP',
                                            summary=appsec_jira_ticket_summury,
                                            description=str(
                                                appsec_jira_ticket_description),
@@ -138,7 +137,7 @@ def submit():
         logging.info("Product created: %s", dojo_name)
 
         # Set Slack notification
-        slack_channels_list = ['#appsec-internal', '#dsp-security']
+        slack_channels_list = ['#zap-test']
         for channel in slack_channels_list:
             slacknotify.slacknotify_jira(slack_token, channel, dojo_name, security_champion,
                                          product_id, dojo_host_url, jira_instance,
@@ -151,7 +150,7 @@ def submit():
         logging.info("Product created: %s", dojo_name)
 
         # When Jira ticket creation is not selected
-        slack_channels_list = ['#appsec-internal', '#dsp-security']
+        slack_channels_list = ['#zap-test']
         for channel in slack_channels_list:
             slacknotify.slacknotify(
                 slack_token, channel, dojo_name, security_champion, product_id, dojo_host_url)
@@ -295,7 +294,7 @@ def request_tm():
 
     logging.info("Request for threat model for project %s ", project_name)
 
-    jira_ticket_appsec = jira.create_issue(project='DSEC',
+    jira_ticket_appsec = jira.create_issue(project='ATP',
                                            summary=appsec_jira_ticket_summury,
                                            description=str(
                                                appsec_jira_ticket_description),
@@ -306,7 +305,8 @@ def request_tm():
     slack_channels_list = ['#dsp-security', '#appsec-internal']
     for channel in slack_channels_list:
         slacknotify.slacknotify_threat_model(slack_token, channel, security_champion,
-                                             request_type, project_name, jira_instance, jira_ticket_appsec, 'DSEC')
+                                             request_type, project_name, jira_instance, jira_ticket_appsec, 'ATP')
+
 
     return ''
 
