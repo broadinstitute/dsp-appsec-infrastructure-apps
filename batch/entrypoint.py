fa2d692a71789a4edad1e4af1bc2e4b76f86a386
Denis Loginov <dinvlad@gmail.com>
2021-03-10 16:38:06 -0500

    [batch] Improve exception logging

--- batch/entrypoint.py
+++ batch/entrypoint.py
@@ -164,8 +164,8 @@ def cleanup(subscription: str, namespace: str) -> None:
                     propagation_policy="Background",
                 )
                 log.info("Deleted job %s", meta.name)
-    except Exception as err:  # pylint: disable=broad-except
-        log.info("Error cleaning up job %s: %s", meta.name, err)
+    except:  # pylint: disable=bare-except
+        log.exception("Error cleaning up job %s", meta.name)
 
 
 def schedule_cleanup(subscription: str, namespace: str) -> None:
