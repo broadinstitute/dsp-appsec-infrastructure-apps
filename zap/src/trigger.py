46c4e40231b43b6efdc2d84677859f72dcce7648
Sarada Symonds <ssymonds@broadinstitute.org>
2021-05-21 13:49:54 -0700

    added traceback for error reportng

--- zap/src/trigger.py
+++ zap/src/trigger.py
@@ -134,7 +134,7 @@ def trigger_scans(
             raise BaseException("testing")
         except BaseException as error:
             logging.error(f"Error triggering scan for: { endpoint }.")
-            print(traceback.print_exc())
+            print(f"{ error }: { traceback.print_exc() })
 
     concurrent.futures.wait(futures)
 
