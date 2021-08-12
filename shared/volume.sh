202cdb73cde122e8df9aa3bb7606d3c0dfd3f80f
Denis Loginov <denis@broadinstitute.org>
2021-08-12 12:26:50 -0400

    [shared] Implement ComputeDisk interface workaround

--- shared/volume.sh
+++ shared/volume.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 ./kube-apply.py "disk.yaml"
 
-# TODO re-enable once this issue is resolved:
-# https://github.com/GoogleCloudPlatform/k8s-config-connector/issues/521
-# kubectl wait --for condition=Ready ComputeDisk \
-#     "${SERVICE_DISK}" -n "${NAMESPACE}" --timeout "180s"
+kubectl wait --for condition=Ready ComputeDisk \
+    "${SERVICE_DISK}" -n "${NAMESPACE}" --timeout "120s"
 
 ./kube-apply.py "volume.yaml"
