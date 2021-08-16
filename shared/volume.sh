daefc44f14278df202047eebb899a61fdde7644c
Denis Loginov <denis@broadinstitute.org>
2021-08-16 16:44:49 -0400

    [shared] Disable ComputeDisk template instead of Volume

--- shared/volume.sh
+++ shared/volume.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-./kube-apply.py "disk.yaml"
-
 # TODO re-enable once this issue is resolved:
 # https://github.com/GoogleCloudPlatform/k8s-config-connector/issues/521
+
+# ./kube-apply.py "disk.yaml"
 
 # kubectl wait --for condition=Ready ComputeDisk \
 #     "${SERVICE_DISK}" -n "${NAMESPACE}" --timeout "180s"
 
-# ./kube-apply.py "volume.yaml"
+./kube-apply.py "volume.yaml"
