a8424a9926d57f20dbd170e33ee872abaf31434f
Zybjana Bedo <zbedo@broadinstitute.org>
2021-09-10 12:46:41 -0400

    [sdarq][frontend] Add & change alerts

--- sdarq/frontend/src/app/form/form.component.ts
+++ sdarq/frontend/src/app/form/form.component.ts
@@ -10,27 +10,37 @@ import formJson from './form.json';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  constructor(private sendForm: SendFormDataService, private scanGCPproject: CisProjectService, private createNewSctService: CreateNewSctService) { }
 
-  ngOnInit() { }
-
+  errors: any;
   json = formJson
   arrRequired = {};
+  showAlert: boolean;
+  showForm: boolean;
+
+  constructor(private sendForm: SendFormDataService, private scanGCPproject: CisProjectService, private createNewSctService: CreateNewSctService) { }
+
+  ngOnInit() {
+    this.showForm = true;
+   }
+
+
 
   sendData(result) {
     this.sendForm.sendFormData(result).subscribe((submitNewServiceQuestionnaireResponse) => {
     },
-      (submitNewServiceQuestionnaireResponse) => { });
+      (submitNewServiceQuestionnaireResponse) => {
+        this.showAlert = true;
+        this.showForm = false;
+        this.errors = submitNewServiceQuestionnaireResponse;
+       });
 
     this.arrRequired = {'service' : result['Service'], 'github': result['Github URL']};
     this.createNewSctService.createNewSCT(this.arrRequired).subscribe((createNewSCTResponse) => {
-    },
-      (createNewSCTResponse) => { });
+    });
 
     if (result.project_id) {
       this.scanGCPproject.sendCisProject(result).subscribe((scanGCPProjectResponse) => {
-      },
-        (scanGCPProjectResponse) => { });
+      });
     }
   }
 }
--- sdarq/frontend/src/app/services/send-form-data.service.ts
+++ sdarq/frontend/src/app/services/send-form-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,8 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 export class SendFormDataService {
 
-  private Url = location.origin + '/submit/';
+  // private Url = location.origin + '/submit/';
+  private Url = 'http://127.0.0.1:8080' + '/submit/';
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +18,26 @@ export class SendFormDataService {
     const options = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     };
-    return this.http.post(this.Url, data, options);
+    return this.http.post(this.Url, data, options).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error) {
+
+    let errorMessage = '';
+
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `${error.error.message}`;
+    } else {
+      // server-side error
+      if (error.error.statusText) {
+        errorMessage = `${error.error.statusText}`;
+      } else {
+        errorMessage = `${error.message}`;
+      }
+    }
+    return throwError(errorMessage);
   }
 }
