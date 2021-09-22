5a46e3a6bafaba643121793328f336d1b616d90b
Zybjana Bedo <zbedo@broadinstitute.org>
2021-09-22 09:14:03 -0400

    [sdarq][ui]

--- sdarq/frontend/src/app/security-controls-list/security-controls-list.component.ts
+++ sdarq/frontend/src/app/security-controls-list/security-controls-list.component.ts
@@ -12,7 +12,7 @@ export class SecurityControlsListComponent implements OnInit {
 
   serviceSecurityControl: ServiceSecurityControl[];
   // tslint:disable-next-line
-  headElements = ['Service', 'Product', 'Contact', 'Dev URL', 'Sourcecode', 'VM', 'DefectDojo', 'Docker scan', 'Manual pentest', 'DAST', 'CIS scan', 'Dependecies scan'];
+  headElements = ['Service', 'Product', 'Contact', 'Dev URL', 'Sourcecode', 'Vulnerability Managment', 'DefectDojo', 'Docker Scan', 'Manual Pentest', 'DAST', 'CIS scan', 'Dependecies scan'];
   sourceclear_results: boolean;
   searchString: any;
 
