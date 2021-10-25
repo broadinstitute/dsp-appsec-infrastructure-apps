631418d886a07e2055fcb4792728e53a51323980
Zybjana Bedo <zbedo@broadinstitute.org>
2021-10-25 17:57:29 -0400

    [sdarq][backend] Add security controls reminder

--- sdarq/backend/src/security-controls-reminder.py
+++ sdarq/backend/src/security-controls-reminder.py
@@ -17,18 +17,31 @@ def get_sec_controls():
     """
     
     db = firestore.Client()
-    data = []
     docs = db.collection('security-controls').stream()
     for doc in docs:
-        data.append(doc.to_dict())
-    
+        service = doc.to_dict()
+        if service.get('sourceclear') is None or service['sourceclear'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'Sourceclear')
+        if service.get('docker_scan') is None or service['docker_scan'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'Docker scan')
+        if service.get('burp') is None or service['burp'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'Security manual pentest')
+        if service.get('zap') is None or service['zap'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'DAST')
+        if service.get('cis_scanner') is None or service['cis_scanner'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'CIS scanner')
+        if service.get('threat_model') is None or service['threat_model'] is False:
+            slacknotify.slacknotify_security_pentest(slack_channel, service['service'], 'Threat Model')
 
 def main():
     """
-    Implements the security-controls-reminder.py test
+    Implements the security-controls-reminder.py
     """
 
 security_controls_firestore_collection = os.environ['SC_FIRESTORE_COLLECTION']
+slack_channel = os.getenv('SLACK_CHANNEL')
+
+get_sec_controls()
 
 if __name__ == "__main__":
     main()
