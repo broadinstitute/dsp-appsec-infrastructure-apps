c27693e55f1dc1cbb4049fb41d8bc94fba4a6494
Zybjana Bedo <zbedo@broadinstitute.org>
2021-11-03 13:29:36 -0400

    [zap][dojo]

--- zap/src/scan.py
+++ zap/src/scan.py
@@ -60,7 +60,7 @@ def defectdojo_upload(engagement_id: int, zap_filename: str, defect_dojo_key: st
     Upload Zap results in DefectDojo engagement
     """
     dojo = defectdojo.DefectDojoAPIv2(
-        defect_dojo, defect_dojo_key, defect_dojo_user, debug=False)
+        defect_dojo, defect_dojo_key, defect_dojo_user, debug=True)
 
     absolute_path = os.path.abspath(zap_filename)
     logging.info("aboslute path: %s", absolute_path)
