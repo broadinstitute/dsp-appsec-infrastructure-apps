43723c109df4cd9620835479544f8a5006c71227
Zybjana Bedo <zbedo@broadinstitute.org>
2022-03-29 17:01:11 -0400

    [sdarq] Fix URLs regex

--- sdarq/backend/src/schemas/threat_model_request_schema.py
+++ sdarq/backend/src/schemas/threat_model_request_schema.py
@@ -7,19 +7,19 @@ tm_schema = {
         },
         "Name": {
             "type": "string",
-            "pattern": "^[a-zA-Z0-9][a-zA-Z0-9-_\s]{1,20}[a-zA-Z0-9]$"
+            "pattern": "^[a-zA-Z0-9][a-zA-Z0-9-_ ]{1,28}[a-zA-Z0-9]$"
         },
         "Github": {
             "type": "string",
-            "pattern": "^[a-zA-Z0-9][a-zA-Z0-9-._\/:]{1,60}[a-zA-Z0-9\\/]$"
+            "pattern": "^(https?://)?[a-z0-9/\\./\\-]+(/[a-zA-Z0-9/\\-/\\/]*)?$"
         },
         "Diagram": {
             "type": "string",
-            "pattern": "^[a-zA-Z0-9][a-zA-Z0-9-._\/:]{1,60}[a-zA-Z0-9\\/]$"
+            "pattern": "^(https?://)?[a-z0-9/\\./\\-]+(/[a-zA-Z0-9/\\-/\\/]*)?$"
         },
         "Document": {
             "type": "string",
-            "pattern": "^[a-zA-Z0-9][a-zA-Z0-9-._\/:]{1,60}[a-zA-Z0-9\\/]$"
+            "pattern": "^(https?://)?[a-z0-9/\\./\\-]+(/[a-zA-Z0-9/\\-/\\/]*)?$"
         },
         "Eng": {
             "type": "string",
