00e2f3371a25e12d92868247cd6624518a3ae219
Zybjana Bedo <zbedo@broadinstitute.org>
2022-04-19 16:03:58 -0400

    Edit frontpage

--- docs/src/website/pages/en/index.js
+++ docs/src/website/pages/en/index.js
@@ -184,18 +184,18 @@ class Index extends React.Component {
 
       const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
 
-      // return (
-      //   <div className="productShowcaseSection paddingBottom">
-      //     <h2>Who is Using This?</h2>
-      //     <p>This project is used by</p>
-      //     <div className="logos">{showcase}</div>
-      //     <div className="more-users">
-      //       <a className="button" href={pageUrl('users.html')}>
-      //         More {siteConfig.title} Users
-      //       </a>
-      //     </div>
-      //   </div>
-      // );
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          {/* <h2>Who is Using This?</h2>
+          <p>This project is used by</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div> */}
+        </div>
+      );
     };
 
     return (
