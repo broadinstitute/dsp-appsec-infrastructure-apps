061f24ad318d84f316785e37a849298051dcd0c9
Zybjana Bedo <zbedo@broadinstitute.org>
2022-10-17 19:31:22 -0400

    Revert "[sdarq] Initial app questionnaire"
    This reverts commit e53e72c2ed965c1b0e7982634a7dec4d59733c04.

--- sdarq/frontend/src/app/app.module.ts
+++ sdarq/frontend/src/app/app.module.ts
@@ -42,7 +42,6 @@ import { JiraTicketRiskAssessmentService } from './services/jira-ticket-risk-ass
 import { FilterPipe } from './pipes/filter.pipe';
 import { FiltersctPipe } from './pipes/filtersct.pipe';
 import { AppRoutingModule } from './app-routing.module';
-import { AppFormComponent } from './app-form/app-form.component';
 
 
 @NgModule({
@@ -71,8 +70,7 @@ import { AppFormComponent } from './app-form/app-form.component';
     EditSecurityControlsFormComponent,
     SecurityPentestComponent,
     SecurityRequestsComponent,
-    SecurityControlsComponent,
-    AppFormComponent
+    SecurityControlsComponent
     ],
   imports: [
     BrowserModule,
