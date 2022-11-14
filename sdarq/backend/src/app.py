6f9a128dfaaefa058ee10590ce8e30b53a523931
zbedo <44617524+zbedo@users.noreply.github.com>
2022-11-14 16:02:27 -0500

    [DSEC-581][sdarq] Track security-related requirements  (#386)
    * [sdarq]
    
    * [sdarq]

--- sdarq/backend/src/app.py
+++ sdarq/backend/src/app.py
@@ -110,84 +110,71 @@ def submit():
         200 status
         400 status
     """
-    json_data = request.get_json()
-    dojo_name = json_data['Service']
-    security_champion = json_data['Security champion']
-    product_type = 1
-    products_endpoint = f"{dojo_host}api/v2/products/"
-    user_email = request.headers.get('X-Goog-Authenticated-User-Email')
-
-    architecture_diagram = json_data['Architecture Diagram']
-    github_url = json_data['Github URL']
-    appsec_jira_ticket_description = github_url + '\n' + architecture_diagram
-    appsec_jira_ticket_summury_tm = 'Threat Model request ' + dojo_name
-    appsec_jira_ticket_summury_srcl = 'Add ' + \
-        dojo_name + ' to 3rd party dependencies scan tool'
-    appsec_jira_ticket_summury_sast = 'Add ' + dojo_name + ' to a SAST tool'
 
     if request.headers.get('Content-Type') != 'application/json':
         return Response(json.dumps(
             {'statusText': 'Bad Request'}), status=400, mimetype='application/json')
 
     try:
-        validate(instance=json_data, schema=new_service_schema)
-        if 'JiraProject' in json_data:
-            project_key_id = json_data['JiraProject']
-            dev_jira_ticket_summury = dojo_name + ' security requirements'
-            jira_description = json.dumps(
+        json_data = request.get_json()
+        dojo_name = json_data['Service']
+        security_champion = json_data['Security champion']
+        product_type = 1
+        products_endpoint = f"{dojo_host}api/v2/products/"
+        user_email = request.headers.get('X-Goog-Authenticated-User-Email')
+
+        architecture_diagram = json_data['Architecture Diagram']
+        github_url = json_data['Github URL']
+        appsec_jira_ticket_description = github_url + '\n' + architecture_diagram
+        appsec_jira_ticket_summury_tm = 'Threat Model request ' + dojo_name
+        appsec_jira_ticket_summury_srcl = 'Add ' + \
+            dojo_name + ' to 3rd party dependencies scan tool'
+        appsec_jira_ticket_summury_sast = 'Add ' + dojo_name + ' to a SAST tool'
+        project_key_id = json_data['JiraProject']
+        dev_jira_ticket_summury_alerts = dojo_name + ' security related requirements'
+        app_jira_ticket_summury_alerts = 'Track ' + dojo_name + ' security related requirements'
+        jira_description = json.dumps(
                 json_data['Ticket_Description']).strip('[]')
 
-            formatted_jira_description = jira_description.strip(
-                '", "').replace('", "', '\n-')
+        validate(instance=json_data, schema=new_service_schema)
 
-            jira_ticket = jiranotify.create_board_ticket(
-                project_key_id, dev_jira_ticket_summury, formatted_jira_description)
+        formatted_jira_description = jira_description.strip(
+            '", "').replace('", "', '\n-')
 
-            logging.info("Jira ticket in %s board created by %s",
-                         project_key_id, user_email)
+        jira_ticket = jiranotify.create_board_ticket(
+            project_key_id, dev_jira_ticket_summury_alerts, formatted_jira_description)
 
-            del json_data['Ticket_Description']
+        logging.info("Jira ticket in %s board created by %s",
+                        project_key_id, user_email)
 
-            data = {
-                'name': dojo_name,
-                'description': parse_json_data.prepare_dojo_input(json_data),
-                'prod_type': product_type}
-            res = requests.post(products_endpoint,
-                                headers=headers, data=json.dumps(data))
-            res.raise_for_status()
-            product_id = res.json()['id']
+        del json_data['Ticket_Description']
 
-            logging.info("Product created: %s by %s request",
-                         dojo_name, user_email)
+        data = {
+            'name': dojo_name,
+            'description': parse_json_data.prepare_dojo_input(json_data),
+            'prod_type': product_type}
+        res = requests.post(products_endpoint,
+                            headers=headers, data=json.dumps(data))
+        res.raise_for_status()
+        product_id = res.json()['id']
 
-            slacknotify.slacknotify_jira(
-                appsec_slack_channel,
-                dojo_name,
-                security_champion,
-                product_id,
-                dojo_host_url,
-                jira_instance,
-                project_key_id,
-                jira_ticket)
-        else:
-            data = {
-                'name': dojo_name,
-                'description': parse_json_data.prepare_dojo_input(json_data),
-                'prod_type': product_type}
-            res = requests.post(products_endpoint,
-                                headers=headers, data=json.dumps(data))
-            res.raise_for_status()
-            product_id = res.json()['id']
+        logging.info("Product created: %s by %s request",
+                        dojo_name, user_email)
 
-            logging.info("Product created: %s by %s request",
-                         dojo_name, user_email)
+        slacknotify.slacknotify_jira(
+            appsec_slack_channel,
+            dojo_name,
+            security_champion,
+            product_id,
+            dojo_host_url,
+            jira_instance,
+            project_key_id,
+            jira_ticket)
 
-            slacknotify.slacknotify(
-                appsec_slack_channel,
-                dojo_name,
-                security_champion,
-                product_id,
-                dojo_host_url)
+        jiranotify.create_board_ticket(
+            appsec_jira_project_key,
+            app_jira_ticket_summury_alerts,
+            formatted_jira_description)
 
         jiranotify.create_board_ticket(
             appsec_jira_project_key,
@@ -231,84 +218,73 @@ def submit_app():
         200 status
         400 status
     """
-    json_data = request.get_json()
-    dojo_name = json_data['Service']
-    security_champion = json_data['Security champion']
-    product_type = 1
-    products_endpoint = f"{dojo_host}api/v2/products/"
-    user_email = request.headers.get('X-Goog-Authenticated-User-Email')
 
-    architecture_diagram = json_data['Architecture Diagram']
-    github_url = json_data['Github URL']
-    appsec_jira_ticket_description = github_url + '\n' + architecture_diagram
-    appsec_jira_ticket_summury_tm = 'Threat Model request ' + dojo_name
-    appsec_jira_ticket_summury_srcl = 'Add ' + \
-        dojo_name + ' to 3rd party dependencies scan tool'
-    appsec_jira_ticket_summury_sast = 'Add ' + dojo_name + ' to a SAST tool'
 
     if request.headers.get('Content-Type') != 'application/json':
         return Response(json.dumps(
             {'statusText': 'Bad Request'}), status=400, mimetype='application/json')
 
     try:
+        json_data = request.get_json()
+        dojo_name = json_data['Service']
+        security_champion = json_data['Security champion']
+        product_type = 1
+        products_endpoint = f"{dojo_host}api/v2/products/"
+        user_email = request.headers.get('X-Goog-Authenticated-User-Email')
+
+        architecture_diagram = json_data['Architecture Diagram']
+        github_url = json_data['Github URL']
+        appsec_jira_ticket_description = github_url + '\n' + architecture_diagram
+        appsec_jira_ticket_summury_tm = 'Threat Model request ' + dojo_name
+        appsec_jira_ticket_summury_srcl = 'Add ' + \
+            dojo_name + ' to 3rd party dependencies scan tool'
+        appsec_jira_ticket_summury_sast = 'Add ' + dojo_name + ' to a SAST tool'
+        project_key_id = json_data['JiraProject']
+        dev_jira_ticket_summury_alerts = dojo_name + ' security requirements'
+        appsec_jira_ticket_summury_alerts = 'Track ' + dojo_name + ' security requirements'
+
+        jira_description = json.dumps(
+            json_data['Ticket_Description']).strip('[]')
+
         validate(instance=json_data, schema=new_app_schema)
-        if 'JiraProject' in json_data:
-            project_key_id = json_data['JiraProject']
-            dev_jira_ticket_summury = dojo_name + ' security requirements'
-            jira_description = json.dumps(
-                json_data['Ticket_Description']).strip('[]')
 
-            formatted_jira_description = jira_description.strip(
-                '", "').replace('", "', '\n-')
+        formatted_jira_description = jira_description.strip(
+            '", "').replace('", "', '\n-')
 
-            jira_ticket = jiranotify.create_board_ticket(
-                project_key_id, dev_jira_ticket_summury, formatted_jira_description)
+        jira_ticket = jiranotify.create_board_ticket(
+            project_key_id, dev_jira_ticket_summury_alerts, formatted_jira_description)
 
-            logging.info("Jira ticket in %s board created by %s",
-                         project_key_id, user_email)
+        logging.info("Jira ticket in %s board created by %s",
+                        project_key_id, user_email)
 
-            del json_data['Ticket_Description']
+        del json_data['Ticket_Description']
 
-            data = {
-                'name': dojo_name,
-                'description': parse_json_data.prepare_dojo_input(json_data),
-                'prod_type': product_type}
-            res = requests.post(products_endpoint,
-                                headers=headers, data=json.dumps(data))
-            res.raise_for_status()
-            product_id = res.json()['id']
+        data = {
+            'name': dojo_name,
+            'description': parse_json_data.prepare_dojo_input(json_data),
+            'prod_type': product_type}
+        res = requests.post(products_endpoint,
+                            headers=headers, data=json.dumps(data))
+        res.raise_for_status()
+        product_id = res.json()['id']
 
-            logging.info("Product created: %s by %s request",
-                         dojo_name, user_email)
+        logging.info("Product created: %s by %s request",
+                        dojo_name, user_email)
 
-            slacknotify.slacknotify_app_jira(
-                appsec_slack_channel,
-                dojo_name,
-                security_champion,
-                product_id,
-                dojo_host_url,
-                jira_instance,
-                project_key_id,
-                jira_ticket)
-        else:
-            data = {
-                'name': dojo_name,
-                'description': parse_json_data.prepare_dojo_input(json_data),
-                'prod_type': product_type}
-            res = requests.post(products_endpoint,
-                                headers=headers, data=json.dumps(data))
-            res.raise_for_status()
-            product_id = res.json()['id']
-
-            logging.info("Product created: %s by %s request",
-                         dojo_name, user_email)
-
-            slacknotify.slacknotify_app(
-                appsec_slack_channel,
-                dojo_name,
-                security_champion,
-                product_id,
-                dojo_host_url)
+        slacknotify.slacknotify_app_jira(
+            appsec_slack_channel,
+            dojo_name,
+            security_champion,
+            product_id,
+            dojo_host_url,
+            jira_instance,
+            project_key_id,
+            jira_ticket)
+            
+        jiranotify.create_board_ticket(
+            appsec_jira_project_key,
+            appsec_jira_ticket_summury_alerts,
+            formatted_jira_description)
 
         jiranotify.create_board_ticket(
             appsec_jira_project_key,
