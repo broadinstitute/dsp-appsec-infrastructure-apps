a7bbbb4f91f40f91622dc76304b19962c91163c0
Zybjana Bedo <zbedo@broadinstitute.org>
2022-12-06 22:13:14 -0500

    Edit deploy script

--- security-controls/deploy.sh
+++ security-controls/deploy.sh
@@ -7,15 +7,13 @@ cd ../shared
 export $(xargs < .env)
 
 export NAMESPACE="security-controls"
-export CRON_JOB="${NAMESPACE}-trigger-weekly"
-export CRON_SERVICE_ACCOUNT="${CRON_JOB}"
 
 ./kube-apply.py \
   "namespace.yaml" \
   "configconnectorcontext.yaml"
 
-export SERVICE="${CRON_JOB}"
-export SERVICE_ACCOUNT="${CRON_SERVICE_ACCOUNT}"
+export CRON_JOB="${NAMESPACE}-trigger-weekly"
+export CRON_SERVICE_ACCOUNT="${CRON_JOB}"
 ./kube-apply.py "service-account.yaml"
 
 ./kube-apply.py "${CWD}/deployment.yaml"
