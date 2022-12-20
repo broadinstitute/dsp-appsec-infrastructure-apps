24bcd46e409416525e3ba781526e6b29ce4634c0
Zybjana Bedo <zbedo@broadinstitute.org>
2022-12-20 16:39:51 -0500

    Change deployment files

--- security-controls/deploy.sh
+++ security-controls/deploy.sh
@@ -7,16 +7,15 @@ cd ../shared
 export $(xargs < .env)
 
 export NAMESPACE="security-controls"
-
-./kube-apply.py \
-  "namespace.yaml" \
-  "configconnectorcontext.yaml"
-
 export DEFECT_DOJO_URL="http://${DOJO_SERVICE}.${DOJO_NAMESPACE}.svc.cluster.local"
 export SERVICE="${NAMESPACE}"
 export SERVICE_SECRET="${NAMESPACE}"
 export CRON_JOB="${NAMESPACE}-trigger-daily"
 export CRON_SERVICE_ACCOUNT="${CRON_JOB}"
+
+./kube-apply.py \
+  "namespace.yaml" \
+  "configconnectorcontext.yaml"
 
 export SERVICE_ACCOUNT="${CRON_SERVICE_ACCOUNT}"
 ./kube-apply.py "service-account.yaml"
