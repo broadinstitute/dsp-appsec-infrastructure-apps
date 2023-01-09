3ff5742af5d6f64fc4f12fe1b69db7de46743f8a
Zybjana Bedo <zbedo@broadinstitute.org>
2023-01-09 18:30:24 -0500

    Add keywords mapping

--- security-controls/notify.py
+++ security-controls/notify.py
@@ -13,24 +13,34 @@ def notify_appsec(security_controls_firestore_collection, slack_token, slack_cha
     db = firestore.Client()
     docs = db.collection(security_controls_firestore_collection).stream()
 
+    keyword_maps = {
+        "burp": "security manual pentest",
+        "zap": "DAST",
+        "cis_scanner": "CIS scanner",
+        "sourceclear": "3rd party dependencies scan",
+        "docker_scan": "container image scan",
+        "threat_model": "threat model",
+        "sast": "SAST"
+    }
+
     for doc in docs:
         for key in doc.to_dict():
             if doc.to_dict()[key] is False:
                 service_seccon =  f"{doc.to_dict()['service']}_{key}"
                 if service_seccon in security_controls_ignore_final_list:
-                    print(security_controls_ignore_final_list)
                     continue
+                
                 client = WebClient(token=slack_token)
                 client.chat_postMessage(
                     channel=slack_channel,
-                    text="SDARQ Security Controls",
+                    text="AppSec reminder",
                     attachments=[{"blocks": [
                         {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
                                 "text":
-                                    f"Security control `{key}` is not integrated for {doc.to_dict()['service']}!",
+                                    f"Security control `{keyword_maps[key]}` is not integrated for {doc.to_dict()['service']}!",
                             }
                         },
                         {
