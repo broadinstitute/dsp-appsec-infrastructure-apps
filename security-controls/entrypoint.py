2cdf9952f37abefe3ee61c10072977e8b17c87f1
Zybjana Bedo <zbedo@broadinstitute.org>
2023-01-09 20:45:16 -0500

    Add pylint and fix pylint findings

--- security-controls/entrypoint.py
+++ security-controls/entrypoint.py
@@ -3,10 +3,10 @@
 This module
 - will get all products that have srlc & zap tag and update security controls column for all services/apps
 """
-import os, logging
+import os
+import logging
 import requests
 from google.cloud import firestore
-
 
 
 def update_dependecies_scan_values(defect_dojo_key: str, defect_dojo: str, security_controls_firestore_collection: str, dep_scan_link: str, defect_dojo_host: str):
@@ -14,26 +14,27 @@ def update_dependecies_scan_values(defect_dojo_key: str, defect_dojo: str, secur
     Get all DefectDojo projects with srcclr tag
     Update all services 3rd party dependencies scan in security controls with the result link to DD
     '''
-    db = firestore.Client()
-    products_endpoint = "{0}/api/v2/products/?tag=srcclr".format(defect_dojo)
+    firestore_docs = firestore.Client()
+    products_endpoint = f"{defect_dojo}/api/v2/products/?tag=srcclr"
 
     headers = {
         "content-type": "application/json",
-        "Authorization": "Token {0}".format(str(defect_dojo_key)),
+        "Authorization": f"Token {str(defect_dojo_key)}",
     }
 
     res = requests.get(products_endpoint,
-                       headers=headers, verify=True)
+                       headers=headers, verify=True, timeout=15)
 
     for product in res.json()['results']:
-        doc_ref = db.collection(
+        doc_ref = firestore_docs.collection(
             security_controls_firestore_collection).document(product['name'].lower())
         doc = doc_ref.get()
         if bool(doc.to_dict()) is True:
-            logging.info("Third party dependecies scan value and link updated for %s ", product['name'])
+            logging.info(
+                "Third party dependecies scan value and link updated for %s ", product['name'])
             doc_ref.set({
-                u'sourceclear': True,
-                u'sourceclear_link': '{0}/product/{1}/finding/{2}'.format(defect_dojo_host, str(product['id']), dep_scan_link)
+                'sourceclear': True,
+                'sourceclear_link': f'{defect_dojo_host}/product/{str(product["id"])}/finding/{dep_scan_link}'
             }, merge=True)
 
 
@@ -42,27 +43,27 @@ def update_dast_values(defect_dojo_key: str, defect_dojo: str, security_controls
     Get all DefectDojo projects with zap tag
     Update all services DAST value in security controls with the result link to DD
     '''
-    db = firestore.Client()
-    products_endpoint = "{0}/api/v2/products/?tag=zap".format(defect_dojo)
+    firestore_docs = firestore.Client()
+    products_endpoint = f"{defect_dojo}/api/v2/products/?tag=zap"
 
     headers = {
         "content-type": "application/json",
-        "Authorization": "Token {0}".format(str(defect_dojo_key)),
+        "Authorization": f"Token {str(defect_dojo_key)}",
     }
 
     res = requests.get(products_endpoint,
-                       headers=headers, verify=True)
+                       headers=headers, verify=True, timeout=15)
     for product in res.json()['results']:
-        doc_ref = db.collection(
+        doc_ref = firestore_docs.collection(
             security_controls_firestore_collection).document(product['name'].lower())
         doc = doc_ref.get()
         if bool(doc.to_dict()) is True:
-            logging.info("DAST value and link updated for %s ", product['name'])
+            logging.info("DAST value and link updated for %s ",
+                         product['name'])
             doc_ref.set({
-                u'zap': True,
-                u'vulnerability_management': '{0}/product/{1}/finding/{2}'.format(defect_dojo_host, str(product['id']), dast_link)
+                'zap': True,
+                'vulnerability_management': f'{defect_dojo_host}/product/{str(product["id"])}/finding/{dast_link}'
             }, merge=True)
-
 
 
 def main():
@@ -79,9 +80,11 @@ def main():
     # configure logging
     logging.basicConfig(level=logging.INFO)
 
-    update_dependecies_scan_values(defect_dojo_key, defect_dojo, security_controls_firestore_collection, dep_scan_link, defect_dojo_host)
+    update_dependecies_scan_values(
+        defect_dojo_key, defect_dojo, security_controls_firestore_collection, dep_scan_link, defect_dojo_host)
 
-    update_dast_values(defect_dojo_key, defect_dojo, security_controls_firestore_collection, dast_link, defect_dojo_host)
+    update_dast_values(defect_dojo_key, defect_dojo,
+                       security_controls_firestore_collection, dast_link, defect_dojo_host)
 
 
 if __name__ == "__main__":
--- security-controls/notify.py
+++ security-controls/notify.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 This module
-- will notify AppSec team in slack for all security controls that are not implemented for all services
+- will notify AppSec team in Slack for all security controls,
+  that are not implemented for all services
 """
 
 import os
@@ -16,8 +17,8 @@ def notify_appsec(security_controls_firestore_collection, slack_token, slack_cha
     Report to a Slack channel
     """
 
-    db = firestore.Client()
-    docs = db.collection(security_controls_firestore_collection).stream()
+    firestore_docs = firestore.Client()
+    docs = firestore_docs.collection(security_controls_firestore_collection).stream()
     keyword_maps = {
         "burp": "security manual pentest",
         "zap": "DAST",
@@ -31,10 +32,10 @@ def notify_appsec(security_controls_firestore_collection, slack_token, slack_cha
     for doc in docs:
         for key in doc.to_dict():
             if doc.to_dict()[key] is False:
-                service_seccon =  f"{doc.to_dict()['service']}_{key}"
+                service_seccon = f"{doc.to_dict()['service']}_{key}"
                 if service_seccon in security_controls_ignore_final_list:
                     continue
-                
+
                 client = WebClient(token=slack_token)
                 client.chat_postMessage(
                     channel=slack_channel,
@@ -52,7 +53,6 @@ def notify_appsec(security_controls_firestore_collection, slack_token, slack_cha
                 )
 
 
-
 def main():
     """
     Implements the entrypoint.
@@ -63,7 +63,8 @@ def main():
     security_controls_ignore_list = os.getenv('SECURITY_CONTROLS_IGNORE', '')
     security_controls_ignore_final_list = security_controls_ignore_list.split(",")
 
-    notify_appsec(security_controls_firestore_collection, slack_token, slack_channel, security_controls_ignore_final_list)
+    notify_appsec(security_controls_firestore_collection, slack_token,
+                  slack_channel, security_controls_ignore_final_list)
 
 
 if __name__ == "__main__":
