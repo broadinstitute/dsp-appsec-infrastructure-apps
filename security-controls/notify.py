b2196b7a2bb691a966a19fcba9f1a05e236156a0
zbedo <44617524+zbedo@users.noreply.github.com>
2023-01-10 15:10:18 -0500

    [DSEC-586] Add alerts for missing security controls (#398)
    * [Sdarq] Add alerts for missing security controls
    
    * [sdarq] Edit deployment file
    
    * [sdarq] Edit syntax
    
    * [security-controls] Trigger build
    
    * Trigger another build
    
    * Edit deployment file
    
    * Add slack_sdk dependency
    
    * Trigger build
    
    * Edit deployment file
    
    * Edit deployment file
    
    * Add keywords mapping
    
    * Set seccon to false when new service/app is creates
    
    * Edit Slack message
    
    * Add pylint and fix pylint findings
    
    * Consider cases when service name has a space for the AllowList
    
    * Trigger build
    
    * Trigger build
    
    * Trigger build
    
    * Trigger build
    
    * Trigger build

---
+++ security-controls/notify.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+This module
+- will notify AppSec team in Slack for all security controls,
+  that are not implemented for all services
+"""
+
+import os
+from google.cloud import firestore
+from slack_sdk import WebClient
+
+
+def notify_appsec(security_controls_firestore_collection, slack_token, slack_channel, security_controls_ignore_final_list):
+    """
+    Get all security controls
+    Check security controls that are set to false and not part of a FP list
+    Report to a Slack channel
+    """
+
+    firestore_docs = firestore.Client()
+    docs = firestore_docs.collection(security_controls_firestore_collection).stream()
+    keyword_maps = {
+        "burp": "security manual pentest",
+        "zap": "DAST",
+        "cis_scanner": "CIS scanner",
+        "sourceclear": "3rd party dependencies scan",
+        "docker_scan": "container image scan",
+        "threat_model": "threat model",
+        "sast": "SAST"
+    }
+
+    for doc in docs:
+        for key in doc.to_dict():
+            if doc.to_dict()[key] is False:
+                service_name = doc.to_dict()['service'].strip(' ').replace(' ','_')
+                service_seccon = f"{service_name}_{key}"
+                print(service_seccon)
+                print(security_controls_ignore_final_list)
+                if service_seccon in security_controls_ignore_final_list:
+                    continue
+
+                client = WebClient(token=slack_token)
+                client.chat_postMessage(
+                    channel=slack_channel,
+                    text="*AppSec Action Required*",
+                    attachments=[{"blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text":
+                                    f"Security control `{keyword_maps[key]}` is not integrated/implemented for `{doc.to_dict()['service'].capitalize()}`.",
+                            }
+                        },
+                    ], "color": "#C31818"}]
+                )
+
+
+def main():
+    """
+    Implements the entrypoint.
+    """
+    security_controls_firestore_collection = os.environ['SC_FIRESTORE_COLLECTION']
+    slack_token = os.getenv('SLACK_TOKEN')
+    slack_channel = os.getenv('SLACK_CHANNEL')
+    security_controls_ignore_list = os.getenv('SECURITY_CONTROLS_IGNORE', '')
+    security_controls_ignore_final_list = security_controls_ignore_list.split(",")
+
+    notify_appsec(security_controls_firestore_collection, slack_token,
+                  slack_channel, security_controls_ignore_final_list)
+
+
+if __name__ == "__main__":
+    main()
