44096d4345cf6d7e6a841c7cb89e45debf186453
zbedo <44617524+zbedo@users.noreply.github.com>
2023-01-26 11:09:16 -0500

    [seccon] Fix env (#402)

--- security-controls/deploy.sh
+++ security-controls/deploy.sh
@@ -9,6 +9,7 @@ export $(xargs < .env)
 export NAMESPACE="security-controls"
 export DEFECT_DOJO_URL="http://${DOJO_SERVICE}.${DOJO_NAMESPACE}.svc.cluster.local"
 export SERVICE_SECRET="${NAMESPACE}"
+export SC_FIRESTORE_COLLECTION="security-controls"
 export SERVICE="${NAMESPACE}-trigger-daily"
 export SERVICE_ACCOUNT="${NAMESPACE}"
 
