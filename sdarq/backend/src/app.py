5d4ef142d428eeb3c094e33bf33b11a529b8382b
Zybjana Bedo <zbedo@broadinstitute.org>
2023-02-09 15:06:36 -0500

    [sdarq]

--- sdarq/backend/src/app.py
+++ sdarq/backend/src/app.py
@@ -731,7 +731,7 @@ def edit_sec_controls():
             if bool(doc.to_dict()) is True:
                 for key in json_data:
                     doc_ref.set({
-                        f'{key}': f'{doc.to_dict()[key]}'
+                        f'{key}': {doc.to_dict()[key]}
                     }, merge=True)
                     logging.info(
                         "Security control %s for the choosen service have changed by %s !",
