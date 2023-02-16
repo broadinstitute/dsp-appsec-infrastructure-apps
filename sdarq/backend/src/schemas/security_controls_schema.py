285741d3aefc8abd9df0914ff460e3051f7281d2
Zybjana Bedo <zbedo@broadinstitute.org>
2023-02-16 14:44:37 -0500

    [sdarq]

--- sdarq/backend/src/schemas/security_controls_schema.py
+++ sdarq/backend/src/schemas/security_controls_schema.py
@@ -17,7 +17,7 @@ security_controls_schema = {
         },
         "dev_url": {
             "type": "string",
-            "pattern": url_pattern
+            "pattern": "^$|^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
         },
         "defect_dojo": {
             "type": "string",
