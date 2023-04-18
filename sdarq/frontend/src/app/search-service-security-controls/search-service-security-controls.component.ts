4e86503572c34e4e58c83ded1860beaf2c678ed8
Zybjana Bedo <zbedo@broadinstitute.org>
2023-04-18 17:47:15 -0400

    [sdarq] Add UI page

--- sdarq/frontend/src/app/search-service-security-controls/search-service-security-controls.component.ts
+++ sdarq/frontend/src/app/search-service-security-controls/search-service-security-controls.component.ts
@@ -11,30 +11,27 @@ import { GetServiceSecurityControlsService } from '../services/get-service-secur
 export class SearchServiceSecurityControlsComponent implements OnInit {
 
   json = formJson;
-  showModal: boolean;
   showForm: boolean;
   showModalErr: boolean;
 
   constructor(private getSecurityControls: GetServiceSecurityControlsService,
-              private ref: ChangeDetectorRef,
-              private ngZone: NgZone) { }
+    private ref: ChangeDetectorRef,
+    private ngZone: NgZone) {}
 
   ngOnInit(): void {
-    this.showModal = false;
     this.showForm = true;
     this.showModalErr = false;
   }
 
-  private getResults(result) {
+  sendData(result) {
     this.getSecurityControls.getServiceSecurityControls(result).subscribe((data) => {
-      this.ref.detectChanges();
-      location.href = location.origin + '/service-security-controls/results?servicename=' + result.service;
-    },
-    (data) => {
-      this.ngZone.run(() => {
-      this.showModal = true;
-      this.showModalErr = data;
-    });
-  });
-}
+        this.ref.detectChanges();
+        location.href = location.origin + '/service-security-controls/results?servicename=' + result.service;
+      },
+      (data) => {
+        this.ngZone.run(() => {
+          this.showModalErr = data;
+        });
+      });
+  }
 }
