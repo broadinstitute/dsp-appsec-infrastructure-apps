d66b8ee3acd1a1ee8ac8d3c6e4b3c4595d8a0800
Zybjana Bedo <zbedo@broadinstitute.org>
2023-04-19 17:28:57 -0400

    [sdarq]

--- sdarq/frontend/src/app/service-security-controls/service-security-controls.component.ts
+++ sdarq/frontend/src/app/service-security-controls/service-security-controls.component.ts
@@ -17,6 +17,23 @@ export class ServiceSecurityControlsComponent implements OnInit {
   valuejson: {};
   headElements = ['Security Control', 'State']
   data: any;
+  service: string;
+  product: string;
+  burp: boolean;
+  security_pentest_link: string;
+  cis_scanner: boolean;
+  dev_url: string;
+  github: string;
+  sourceclear: boolean;
+  sourceclear_link: string;
+  threat_model: boolean;
+  threat_model_link: string;
+  docker_scan: boolean;
+  zap: boolean;
+  vulnerability_management: string;
+  defect_dojo: string;
+  sast: boolean;
+  sast_link: string;
 
   constructor(private getSecurityControls: GetServiceSecurityControlsService,
     private ngZone: NgZone,
@@ -39,8 +56,23 @@ export class ServiceSecurityControlsComponent implements OnInit {
     this.getSecurityControls.getServiceSecurityControls(this.valuejson).subscribe((serviceSecurityControl) => {
         this.ref.detectChanges();
         this.showSpinner = false;
-        this.data = serviceSecurityControl
-        console.log(this.data)
+        this.sast_link = serviceSecurityControl.sast_link;
+        this.github = serviceSecurityControl.github;
+        this.sourceclear = serviceSecurityControl.sourceclear;
+        this.vulnerability_management = serviceSecurityControl.vulnerability_management;
+        this.product = serviceSecurityControl.product;
+        this.sourceclear_link = serviceSecurityControl.sourceclear_link;
+        this.sast = serviceSecurityControl.sast;
+        this.zap = serviceSecurityControl.zap;
+        this.defect_dojo = serviceSecurityControl.defect_dojo;
+        this.service = serviceSecurityControl.service;
+        this.dev_url = serviceSecurityControl.dev_url;
+        this.threat_model = serviceSecurityControl.threat_model;
+        this.threat_model_link = serviceSecurityControl.threat_model_link;
+        this.docker_scan = serviceSecurityControl.docker_scan;
+        this.cis_scanner = serviceSecurityControl.cis_scanner;
+        this.burp = serviceSecurityControl.burp;
+        this.security_pentest_link = serviceSecurityControl.security_pentest_link;
       },
       (serviceSecurityControl) => {
         this.ngZone.run(() => {
