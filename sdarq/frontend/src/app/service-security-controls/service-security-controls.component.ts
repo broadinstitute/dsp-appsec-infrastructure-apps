5177dc9ebe756f69e06f3ff8e569073569eaa8e2
Zybjana Bedo <zbedo@broadinstitute.org>
2023-04-19 17:00:42 -0400

    [sdarq]Build structure of security controls for one service

--- sdarq/frontend/src/app/service-security-controls/service-security-controls.component.ts
+++ sdarq/frontend/src/app/service-security-controls/service-security-controls.component.ts
@@ -8,32 +8,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./service-security-controls.component.css']
 })
 export class ServiceSecurityControlsComponent implements OnInit {
-  service: string;
-  dev_url: string;
-  threat_model_link: string;
-  threat_model: boolean;
-  docker_scan: boolean;
-  cis_scanner: boolean;
-  burp: boolean;
-  security_pentest_link: string;
   errorMessage: any;
   showServiceData: boolean;
   showModalError: boolean;
   showSpinner: boolean;
   value: string;
   errors: string;
-  zap: boolean;
-  vulnerability_management: string;
-  sourceclear_link: string;
-  defect_dojo: string;
-  github: string;
-  product: string;
-  sast: boolean;
-  sast_link: string;
-  sourceclear: boolean;
   valuejson: {};
   headElements = ['Security Control', 'State']
-  data: { sast_link: string; dev_url: string; github: string; sourceclear: string; cis_scanner: string; burp: string; vulnerability_management: string; product: string; threat_model: string; sourceclear_link: string; sast: boolean; zap: string; security_pentest_link: string; service: string; docker_scan: string; defect_dojo: string; };
+  data: { sast_link: string; dev_url: string; github: string; sourceclear: string; cis_scanner: string; burp: string; vulnerability_management: string; product: string; threat_model: boolean;  threat_model_link: string; sourceclear_link: string; sast: boolean; zap: string; security_pentest_link: string; service: string; docker_scan: string; defect_dojo: string; };
 
 
   constructor(private getSecurityControls: GetServiceSecurityControlsService,
