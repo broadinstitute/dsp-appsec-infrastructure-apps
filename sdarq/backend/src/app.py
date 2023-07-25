32e967f953f171ef7acab937e9fc990d2c87a988
Zybjana Bedo <zbedo@broadinstitute.org>
2023-07-25 12:35:20 -0400

    [sfarq][backend] Edit

--- sdarq/backend/src/app.py
+++ sdarq/backend/src/app.py
@@ -145,12 +145,9 @@ def submit():
 
         validate(instance=json_data, schema=new_service_schema)
 
-        formatted_jira_description = jira_description.strip(
-            '", "').replace('", "', '\n-')
-
-        del json_data['Ticket_Description']
-
         product_id = dojo_helper.dojo_create_or_update(dojo_name, parse_json_data.prepare_dojo_input(json_data), product_type, user_email, appsec_slack_channel, security_champion, dojo_host_url, jira_instance, project_key_id, jira_ticket)
+
+        formatted_jira_description = jira_description.strip( '", "').replace('", "', '\n-')
 
         jira_ticket = jiranotify.create_board_ticket(
             project_key_id,
@@ -159,7 +156,9 @@ def submit():
 
         logging.info("Jira ticket in %s board created by %s",
                      project_key_id, user_email)
-        
+
+        del json_data['Ticket_Description']
+
         jiranotify.create_board_ticket(
             appsec_jira_project_key,
             app_jira_ticket_summury_alerts,
