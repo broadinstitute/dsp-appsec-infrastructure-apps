e3913bf3705775f73d71f82a1da119c6730f5e14
sarahgibs <sgibson@broadinstitute.org>
2023-07-25 17:33:22 -0400

    Increase timeout

--- zap/src/zap.py
+++ zap/src/zap.py
@@ -104,7 +104,7 @@ def leo_auth(host, path, token):
     logging.info("Using the setCookie endpoint to set the cookie.")
     set_cookie_endpoint = f"https://{host}/{target_dir}//setCookie"
     headers = {"Authorization": token}
-    response = requests.get(set_cookie_endpoint, headers=headers, timeout=10)
+    response = requests.get(set_cookie_endpoint, headers=headers, timeout=25)
     if response.status_code == 204:
         logging.info("Set cookie was successful")
         return True
