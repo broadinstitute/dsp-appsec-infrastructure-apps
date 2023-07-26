6280a111541b2176859aae0a11e91615585705cb
sarahgibs <sgibson@broadinstitute.org>
2023-07-26 12:42:44 -0400

    use zap as proxy

--- zap/src/zap.py
+++ zap/src/zap.py
@@ -18,22 +18,24 @@ from zapv2 import ZAPv2
 
 TIMEOUT_MINS = 5
 
+zap_port = int(os.getenv("ZAP_PORT", ""))
+proxy = f"http://localhost:{zap_port}"
 
-def zap_connect(zap_port: int):
+
+def zap_connect():
     """
     Connect to the Zap instance
     """
-    proxy = f"http://localhost:{zap_port}"
     zap = ZAPv2(proxies={"http": proxy, "https": proxy})
     wait_for_zap_start(zap, timeout_in_secs=TIMEOUT_MINS * 60)
     return zap
 
 
-def zap_init(zap_port: int, target_url: str):
+def zap_init(target_url: str):
     """
     Connect to ZAP service running on localhost.
     """
-    zap = zap_connect(zap_port)
+    zap = zap_connect()
     zap.core.new_session(name='zap_session', overwrite=True)
     logging.info("Accessing target %s", target_url)
     # replace with api call to core.accessUrl
@@ -89,6 +91,10 @@ def leo_auth(host, path, token):
     """
     Set up cookie auth for leo apps.
     """
+    proxies = {
+            'http': proxy,
+            'https': proxy,
+            }
     logging.info("Authenticating to Leo...")
     # Leo apps if already launched have a separate domain from Leo.
     # And there's a workspace id after the host. https://custom.host/workspaceId/apiEndPoints
@@ -104,7 +110,7 @@ def leo_auth(host, path, token):
     logging.info("Using the setCookie endpoint to set the cookie.")
     set_cookie_endpoint = f"https://{host}/{target_dir}//setCookie"
     headers = {"Authorization": token}
-    response = requests.get(set_cookie_endpoint, headers=headers, timeout=25)
+    response = requests.get(set_cookie_endpoint, headers=headers, timeout=25, proxies=proxies)
     if response.status_code == 204:
         logging.info("Set cookie was successful")
         return True
@@ -212,7 +218,6 @@ def zap_save_session(zap: ZAPv2, project: str, scan_type: ScanType):
 
 def zap_compliance_scan(
     project: str,
-    zap_port: int,
     target_url: str,
     scan_type: ScanType = ScanType.BASELINE,
 ):
@@ -222,7 +227,7 @@ def zap_compliance_scan(
 
     host, _, path = parse_url(target_url)
 
-    zap = zap_init(zap_port, target_url)
+    zap = zap_init(target_url)
     env = "prod" #set prod as default
     if "dev" in host:
         env = "dev"
