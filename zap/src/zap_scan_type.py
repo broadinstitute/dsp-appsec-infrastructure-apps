e59fb11900a6742cf5c315a6a1c992ccc115a5d4
sarahgibs <sgibson@broadinstitute.org>
2023-08-04 10:16:13 -0400

    refactor scan type

---
+++ zap/src/zap_scan_type.py
@@ -0,0 +1,24 @@
+"""
+Provides the ScanType Enum.
+"""
+
+from enum import Enum
+
+
+class ScanType(str, Enum):
+    """
+    Enumerates Zap compliance scan types
+    """
+
+    API = "API"
+    AUTH = "Authenticated"
+    BASELINE = "Baseline"
+    UI = "UI"
+    LEOAPP = "LeoApp"
+
+    def __str__(self):
+        return str(self.name).lower()
+
+    def label(self):
+        """"Get user-friendly name of the scan type"""
+        return str(self.value)
