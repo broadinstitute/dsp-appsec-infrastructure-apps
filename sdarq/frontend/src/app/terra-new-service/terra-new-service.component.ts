182d067559dc58c43db1b53511f9414f82d30da4
Zybjana Bedo <zbedo@broadinstitute.org>
2023-11-16 11:47:13 -0500

    [sdarq]

--- sdarq/frontend/src/app/terra-new-service/terra-new-service.component.ts
+++ sdarq/frontend/src/app/terra-new-service/terra-new-service.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { SendFormDataService } from '../services/create-new-service/send-form-data.service';
+import { CisProjectService } from '../services/scan-gcp-project/cis-project.service';
+import { CreateNewSctService } from '../services/create-new-security-controls/create-new-sct.service';
+import formJson from './form.json';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-terra-new-service',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerraNewServiceComponent implements OnInit {
 
-  constructor() { }
+  errors: string;
+  json = formJson
+  arrRequired = {};
+  showAlert: boolean;
+  showForm: boolean;
 
-  ngOnInit(): void {
+  constructor(private sendForm: SendFormDataService,
+    private scanGCPproject: CisProjectService,
+    private createNewSctService: CreateNewSctService,
+    private ngZone: NgZone,
+    private ref: ChangeDetectorRef) { 
+      // This is intentional
+    }
+
+    ngOnInit() {
+      this.showForm = true;
+    }
+  
+    sendData(result) {
+      this.sendForm.sendFormData(result).subscribe(() => {
+        this.ref.detectChanges();
+      },
+        (submitNewServiceQuestionnaireResponse) => {
+          this.ngZone.run(() => {
+          this.showAlert = true;
+          this.showForm = false;
+          this.errors = submitNewServiceQuestionnaireResponse;
+        });
+      });
+  
+      this.arrRequired = {
+        'service': result['Service'],
+        'github': result['Github URL'],
+        'product': result['Product'],
+        'dev_url': '',
+        'burp': false,
+        'zap': false,
+        'cis_scanner': false,
+        'sourceclear': false,
+        'docker_scan': false,
+        'threat_model': false,
+        'sast': false
+      };
+      
+      this.createNewSctService.createNewSCT(this.arrRequired).subscribe((createNewSCTResponse) => {
+        console.log("Security Controls template created for this service")
+      });
+  
+      if (result.project_id) {
+        this.scanGCPproject.sendCisProject(result).subscribe((scanGCPProjectResponse) => {
+          console.log("CIS scanner running against GCP project")
+        });
+      }
+    }
   }
-
-}
+  
