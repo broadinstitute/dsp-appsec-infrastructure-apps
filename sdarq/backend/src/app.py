8dba41dc41af76d4f10cb351d931138877249303
Zybjana Bedo <zbedo@broadinstitute.org>
2023-11-22 11:16:53 -0500

    [cis][sdarq]

--- sdarq/backend/src/app.py
+++ sdarq/backend/src/app.py
@@ -422,7 +422,7 @@ def cis_scan():
             doc_ref = db.collection(firestore_collection).document(user_proj)
             doc_ref.delete()
             doc_watch = doc_ref.on_snapshot(on_snapshot)
-            callback_done.wait(timeout=8000)
+            callback_done.wait(timeout=800)
             doc_watch.unsubscribe()
             doc = doc_ref.get()
 
