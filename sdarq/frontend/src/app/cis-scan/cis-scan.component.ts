39b76682b872fa0dbdd5e53e81a0f3090d028913
Zybjana Bedo <zbedo@broadinstitute.org>
2023-11-22 16:06:37 -0500

    [sdarq]

--- sdarq/frontend/src/app/cis-scan/cis-scan.component.ts
+++ sdarq/frontend/src/app/cis-scan/cis-scan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import formJson from './form.json';
 import { CisProjectService } from '../services/scan-gcp-project/cis-project.service';
 
@@ -19,7 +19,7 @@ export class CisScanComponent implements OnInit {
   showModalErr: boolean;
   showModalError: string;
 
-  constructor(private sendProject: CisProjectService, private ngZone: NgZone) {
+  constructor(private sendProject: CisProjectService, private ngZone: NgZone, private ref: ChangeDetectorRef) {
     // This is intentional
    }
 
@@ -30,9 +30,11 @@ export class CisScanComponent implements OnInit {
   }
 
   sendData(result) {
+    this.ref.detectChanges();
     this.showSpinner = true;
     if (result.slack_channel) {
       this.sendProject.sendCisProject(result).subscribe((data) => {
+        this.ref.detectChanges();
         this.showModal = true;
         this.showForm = false;
         this.showSpinner = false;
