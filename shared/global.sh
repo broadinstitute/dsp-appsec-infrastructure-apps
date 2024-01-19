d8fef567937adcf8e33addc2f78bb61825b5204a
zbedo <44617524+zbedo@users.noreply.github.com>
2024-01-19 12:05:10 -0500

    Remove unused config files (#513)

--- shared/global.sh
+++ shared/global.sh
@@ -14,11 +14,6 @@ export NAMESPACE="${GLOBAL_NAMESPACE}"
 export PSP_ROLE="${NAMESPACE}-psp"
 export PSP_BINDING="${PSP_ROLE}"
 
-# kubectl apply -f \
-#   "https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/policy/${PSP_NAME}-psp.yaml"
-
-# kubectl apply -f \
-#   "https://raw.githubusercontent.com/broadinstitute/dsp-appsec-infrastructure-apps/eb1c7d0e2f40080cf3448dafef69f979bf37641c/shared/psa.yaml"
 
 ./kube-apply.py \
   "configconnector.yaml" \
