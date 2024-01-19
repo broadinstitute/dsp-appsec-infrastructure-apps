bc4e61106c7ace4ba77c3f14b069368614f775b9
zbedo <44617524+zbedo@users.noreply.github.com>
2024-01-19 11:56:12 -0500

    Move from PodSecurityPolicy to PodSecurityAdmissionController (#506)
    * Add restricted-pdp config file with updated API version
    
    * Update global.sh
    
    * Update global.sh
    
    * Try Pod security admission controller
    
    * Try Pod security admission controller
    
    * Try Pod security admission controller
    
    * Try Pod security admission controller
    
    * Try V1 for 1.25 version
    
    * Trigger

--- shared/global.sh
+++ shared/global.sh
@@ -10,15 +10,17 @@ gcloud container clusters get-credentials --region "${region}" "${cluster}"
 
 # deploy global resources
 export NAMESPACE="${GLOBAL_NAMESPACE}"
-export PSP_NAME="restricted"
+# export PSP_NAME="restricted"
 export PSP_ROLE="${NAMESPACE}-psp"
 export PSP_BINDING="${PSP_ROLE}"
 
-kubectl apply -f \
-  "https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/policy/${PSP_NAME}-psp.yaml"
+# kubectl apply -f \
+#   "https://raw.githubusercontent.com/kubernetes/website/main/content/en/examples/policy/${PSP_NAME}-psp.yaml"
+
+# kubectl apply -f \
+#   "https://raw.githubusercontent.com/broadinstitute/dsp-appsec-infrastructure-apps/eb1c7d0e2f40080cf3448dafef69f979bf37641c/shared/psa.yaml"
 
 ./kube-apply.py \
-  "psp.yaml" \
   "configconnector.yaml" \
   "namespace.yaml" \
   "configconnectorcontext.yaml" \
