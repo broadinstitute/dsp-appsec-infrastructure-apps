2348423944ec3231d165dd1ca32958448b685ec0
Zybjana Bedo <zbedo@broadinstitute.org>
2024-02-03 23:32:29 -0500

    triger build

--- sdarq/frontend/src/app/authz.guard.ts
+++ sdarq/frontend/src/app/authz.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthzService } from './services/authz/authz.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,17 @@ export class AuthzGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.authzService.fetchUserDetails()) {
-        this.router.navigate(['/']);
-        console.log("false :)")
-        return false;
-      }
-      console.log("true :)")
-      return true;
+    return this.authzService.fetchUserDetails().pipe(
+      map(isAuthorized => {
+        if (!isAuthorized) {
+          this.router.navigate(['/']);
+          console.log("false :)");
+          return false;
+        }
+        console.log("true :)");
+        return true;
+      })
+    );
   }
 }
 
