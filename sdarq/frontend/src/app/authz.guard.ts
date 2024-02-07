5da74517a1eb0979c9fb085f7ee98b23b5f1cd84
Zybjana Bedo <zbedo@broadinstitute.org>
2024-02-07 12:59:24 -0500

    [sdarq]

--- sdarq/frontend/src/app/authz.guard.ts
+++ sdarq/frontend/src/app/authz.guard.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthzService } from './services/authz/authz.service';
-import { map } from 'rxjs/operators';
-import { HttpResponse } from '@angular/common/http';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AuthzService } from './services/authz/authz.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthzGuard implements CanActivate {
+
   constructor(private authzService: AuthzService, private router: Router) {}
-  
-  statusreturned: boolean;
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.authzService.fetchUserDetails().pipe(
-      map((response: HttpResponse<any>) => {
+      map(response => {
         if (response.status === 200) {
-          console.log("True")
           return true;
-        } else if (response.status === 403) {
-          console.log("False")
-          this.router.navigate(['/']);
+        } else {
+          this.router.navigate(['/']); 
           return false;
         }
+      }),
+      catchError((error) => {
+        if (error.status === 403) {
+          this.router.navigate(['/']);
+        }
+        return of(false);
       })
     );
   }
 }
-
