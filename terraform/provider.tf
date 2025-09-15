47c55fadc1ab96fd9e4879fece2f6249c82846e1
sarahgibs <93540258+sarahgibs@users.noreply.github.com>
2025-09-15 12:01:58 -0400

    [dsec-980] Quarterly update of defectdojo image (#626)
    * try latest
    
    * remove zap_common
    
    * after terraform init
    
    * upgrading terraform providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,17 +6,18 @@ terraform {
   required_providers {
     google = {
       source  = "google"
-      version = ">= 5.38.0, < 6.0"
+      version = "> 6.0, <=7.2.0"
     }
 
     google-beta = {
       source  = "google-beta"
-      version = ">= 5.38.0, < 6.0"
+      version = "> 6.0, <=7.2.0"
     }
   }
 }
 
 provider "google" {
+  add_terraform_attribution_label = false
   project = var.project
   region  = var.region
 }
